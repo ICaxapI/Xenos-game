3e9c3a6db1fce49a08773f7ab92624bf5f9510e6
ICaxapI <78122700@mail.ru>
2017-04-05 13:46:07 +0300

    Добавлен класс анимации

---
+++ Xenos/core/src/com/exgames/xenos/Animator.java
@@ -0,0 +1,41 @@
+package com.exgames.xenos;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Created by Alex on 05.04.2017.
+ */
+public class Animator {
+    private static final int FRAME_COLS = 6; // #1
+    private static final int FRAME_ROWS = 5; // #2
+
+    private Animation walkAnimation; // #3
+    private Texture walkSheet; // #4
+    private TextureRegion[] walkFrames; // #5
+    private TextureRegion currentFrame; // #7
+    private float stateTime; // #8
+
+    public Animator() {
+        walkSheet = new Texture(Gdx.files.internal("animation_sheet.png")); // #9
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_ROWS); // #10
+        walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+        walkAnimation = new Animation(0.025f, walkFrames); // #11
+        stateTime = 0f; // #13
+    }
+
+    public TextureRegion needrender() {
+        stateTime += Gdx.graphics.getDeltaTime(); // #15
+        currentFrame = (TextureRegion) walkAnimation.getKeyFrame(stateTime, true); // #16
+        return currentFrame;
+    }
+
+}
--- Xenos/core/src/com/exgames/xenos/Main.java
+++ Xenos/core/src/com/exgames/xenos/Main.java
@@ -9,23 +9,68 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Animator runner;
+	Animator runner1;
+	Animator runner2;
+	Animator runner3;
+	Animator runner4;
+	Animator runner5;
+	Animator runner6;
+	Animator runner7;
+	Animator runner8;
+	Animator runner9;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("./assets/final.png");
+		img = new Texture(Gdx.files.internal("FinalNEW.png"));
+		runner = new Animator();
+		runner1 = new Animator();
+		runner2 = new Animator();
+		runner3 = new Animator();
+		runner4 = new Animator();
+		runner5 = new Animator();
+		runner6 = new Animator();
+		runner7 = new Animator();
+		runner8 = new Animator();
+		runner9 = new Animator();
 
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(runner.needrender(), 50, 50); // #17
+		batch.draw(runner1.needrender(), 100, 50); // #17
+		batch.draw(runner2.needrender(), 50, 100); // #17
+		batch.draw(runner3.needrender(), 0, 0); // #17
+		batch.draw(runner4.needrender(), 200, 200); // #17
+		batch.draw(runner5.needrender(), 200, 50); // #17
+		batch.draw(runner6.needrender(), 50, 200); // #17
+		batch.draw(runner7.needrender(), 150, 150); // #17
+		batch.draw(runner8.needrender(), 150, 50); // #17
+		batch.draw(runner9.needrender(), 50, 150); // #17
 		batch.end();
 	}
-	
+
+	@Override
+	public void resize (int width, int height) {
+		System.out.println("Изменён размер! Теперь он " + width + " на " + height);
+	}
+
+	@Override
+	public void pause () {
+
+	}
+
+	@Override
+	public void resume () {
+
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
