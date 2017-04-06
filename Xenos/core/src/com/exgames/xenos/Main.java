4e7f8713717019321c0aabbf6e7a0f655e62fa5a
ICaxapI <78122700@mail.ru>
2017-04-06 23:06:28 +0300

    Доделка меню и ресурсная оптимизация

--- Xenos/core/src/com/exgames/xenos/Main.java
+++ Xenos/core/src/com/exgames/xenos/Main.java
@@ -7,23 +7,33 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import javax.swing.*;
+
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture logotex;
+	Texture buttonstex;
 	Animator runner;
 	Sprite logo;
 	float fade = 0f;
-	boolean notcomplete = true;
+	boolean completelogo = false;
+	boolean logoexit = false;
+	public Timer timer = new Timer(1000, e -> {
+		completelogo = true;
+	});
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		//runner = new Animator("animation_sheet.png");
+
 		logotex = new Texture(Gdx.files.internal("FinalNEW.png"));
 		logotex.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 		logo = new Sprite(logotex);
 		logo.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getWidth()/2);
 		logo.setPosition(0,((Gdx.graphics.getHeight()-(Gdx.graphics.getWidth()/2))/2));
+
+		buttonstex = new Texture(Gdx.files.internal("buttons/atlasbuttons.png"));
 	}
 
 	@Override
@@ -31,27 +41,38 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		logo.draw(batch, fade);
+		whatRenderNow();
+        batch.draw(buttonstex, 0,0);
 		batch.end();
-		fadeswitch(notcomplete);
 	}
+	public void whatRenderNow(){
+		if (!logoexit) {
+			showlogo(completelogo);
+		} else {
 
-	public void fadeswitch(boolean in){
-		if (in) {
+		}
+	}
+	public void showlogo(boolean in){
+		if (!in & !logoexit) {
 			if (fade < 1) {
+				logo.draw(batch, fade);
 				fade = fade + 0.01f;
 			}
 			if (fade >= 1) {
 				fade = 1;
-				notcomplete = false;
+				logo.draw(batch, fade);
+				timer.start();
 			}
-		} else {
+		} else if (!logoexit) {
 			if (fade <= 1) {
+				logo.draw(batch, fade);
 				fade = fade - 0.01f;
 			}
 			if (fade <= 0) {
 				fade = 0;
-				notcomplete = true;
+				timer.stop();
+				completelogo = false;
+				logoexit = true;
 			}
 		}
 	}
